2675e0e4bcf7c4cc63cf0feeb8666ad77396c3fb
Tuyên Phạm <DT050236@actvn.edu.vn>
2023-12-04 15:36:32 +0700

    update responsive

--- src/infrastructure/common/layout/MainLayout.tsx
+++ src/infrastructure/common/layout/MainLayout.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Layout } from 'antd';
+import { Layout } from "antd";
 import LeftMenu from "src/infrastructure/common/layout/LeftMenu";
 import Header from "src/infrastructure/common/layout/Header";
 import Content from "src/infrastructure/common/layout/Content";
-import styles from 'assets/styles/common/layout/MainLayout.module.css'
+import styles from "assets/styles/common/layout/MainLayout.module.css";
 import LocalStorageService from "src/infrastructure/services/LocalStorageService";
 import Constant from "src/core/application/common/constants";
 import { NextRouter, useRouter } from "next/router";
@@ -11,91 +11,99 @@ import { FullPageLoading } from "../components/controls/loading";
 import Cookie from "src/core/application/common/models/Cookie";
 import Endpoint from "src/core/application/common/Endpoint";
 import { AccountManagementService } from "src/infrastructure/identity/account/service/AccountManagementService";
-import { ProfileState, RolesState, UserIdState } from "src/core/application/common/atoms/identity/account/ProfileState";
+import {
+  ProfileState,
+  RolesState,
+  UserIdState,
+} from "src/core/application/common/atoms/identity/account/ProfileState";
 import SuccessResponse from "src/core/application/dto/common/responses/SuccessResponse";
 import { setRecoilStateAsync } from "../libs/recoil-outside/Service";
 import dynamic from "next/dynamic";
+import LoggerService from "src/infrastructure/services/LoggerService";
+import InvalidModelStateResponse from "src/core/application/dto/common/responses/InvalidModelStateResponse";
 
-const DynamicComponentWithNoSSR = dynamic(
-    () => import('./LeftMenu'),
-    { ssr: false }
-)
+const DynamicComponentWithNoSSR = dynamic(() => import("./LeftMenu"), {
+  ssr: false,
+});
+
 const getMyProfileAsync = async (
-
-    cookie: Cookie,
-    // loggerService: LoggerService,
-    setLoading: Function
+  cookie: Cookie,
+  loggerService: LoggerService,
+  setLoading: Function
 ) => {
-    setLoading(true);
-    let response = await new AccountManagementService().getMyProfileAsync(
-        Endpoint.AccountManagement.getMyProfile,
-        {},
-        cookie
-    );
-    if (response.status == 200) {
-        setRecoilStateAsync(ProfileState, {
-            data: (response as SuccessResponse)?.data.getMyProfile.user,
-        });
-        setRecoilStateAsync(UserIdState, {
-            data: (response as SuccessResponse)?.data.getMyProfile.user.userId,
-        });
+  let response = await new AccountManagementService().getMyProfileAsync(
+    Endpoint.AccountManagement.getMyProfile,
+    {},
+    cookie
+  );
+  if (response.status == 200) {
+    setRecoilStateAsync(ProfileState, {
+      data: (response as SuccessResponse)?.data.getMyProfile.user,
+    });
+    setRecoilStateAsync(UserIdState, {
+      data: (response as SuccessResponse)?.data.getMyProfile.user.userId,
+    });
 
-        setRecoilStateAsync(RolesState, {
-            data: (response as SuccessResponse)?.data.getMyProfile.user.role,
-        });
+    setRecoilStateAsync(RolesState, {
+      data: (response as SuccessResponse)?.data.getMyProfile.user.role,
+    });
 
-        setTimeout(() => {
-            setLoading(false);
-        }, 0);
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }
 
-    };
-    return response;
-}
-
+  if (response.constructor.name == InvalidModelStateResponse.name) {
+    setLoading(false);
+    loggerService.info((response as InvalidModelStateResponse).errors);
+  }
+  return response;
+};
 
 const MainLayout = ({ context, translator, ...props }: any) => {
+  const [isHiddenLeftMenu, setIsHiddenLeftMenu] = useState(false);
+  const router = useRouter();
+  let localStorage = new LocalStorageService();
+  let storage = localStorage.readStorage(Constant.API_TOKEN_STORAGE);
+  const loggerService = new LoggerService();
+  const [isLoading, setIsLoading] = useState(true);
 
-    const router = useRouter();
-    let localStorage = new LocalStorageService();
-    let storage = localStorage.readStorage(Constant.API_TOKEN_STORAGE);
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        if (!storage?.isAuthenticated) {
-            router.push('/account/sign-in.html');
-        }
-    }, [router, storage]);
+  useEffect(() => {
+    if (!storage.isAuthenticated) {
+      router.push("/account/sign-in.html");
+    }
+  }, [router, storage]);
+
+ 
+  const getMyProfile = async () => {
+    await getMyProfileAsync(context, loggerService, setIsLoading);
+  };
+
+  useEffect(() => {
+    getMyProfile();
+  }, []);
 
 
+  if (isLoading) {
+    return <FullPageLoading isLoading={isLoading} />;
+  }
 
-    const getMyProfile = async () => {
-        await getMyProfileAsync(
-            context,
-            setIsLoading
-        )
+   if(!storage.isAuthenticated){
+    return <FullPageLoading isLoading={true} />;
     }
 
-    useEffect(() => {
-        getMyProfile();
-    }, []);
-
-    if (isLoading) {
-        return (
-            <FullPageLoading isLoading={isLoading} />
-        )
-    }
-
-    return (
-        <Layout className={styles.qlhs_main_layout}>
-            <LeftMenu context={context} translator={translator} />
-            <Layout className={styles.qlhs_main_content}>
-                <Header context={context} translator={translator} />
-                <Content context={context} translator={translator}>
-                    {props.children}
-                </Content>
-                {/* <Footer context={context} translator={translator} /> */}
-            </Layout>
-        </Layout>
-    )
+  return (
+    <Layout className={styles.qlhs_main_layout}>
+      <LeftMenu context={context} translator={translator} isHiddenLeftMenu={isHiddenLeftMenu} setIsHiddenLeftMenu={setIsHiddenLeftMenu} />
+      <Layout className={styles.qlhs_main_content}>
+        <Header context={context} translator={translator} isHidden={isHiddenLeftMenu} setIsHidden={setIsHiddenLeftMenu} />
+        <Content context={context} translator={translator}>
+          {props.children}
+        </Content>
+        {/* <Footer context={context} translator={translator} /> */}
+      </Layout>
+    </Layout>
+  );
 };
 
 export default MainLayout;
--- src/infrastructure/services/RequestService.ts
+++ src/infrastructure/services/RequestService.ts
@@ -1,179 +1,218 @@
-import { setRecoilStateAsync } from './../common/libs/recoil-outside/Service';
-import Constants from 'src/core/application/common/constants';
+import { setRecoilStateAsync } from "./../common/libs/recoil-outside/Service";
+import Constants from "src/core/application/common/constants";
 import { IRequestService } from "src/core/application/services/IRequestService";
 import LoggerService from "src/infrastructure/services/LoggerService";
 import LocalStorageService from "src/infrastructure/services/LocalStorageService";
 import Cookie from "src/core/application/common/models/Cookie";
-import { RequestResponse } from 'src/core/application/dto/common/responses/RequestResponse';
-import FailureResponse from 'src/core/application/dto/common/responses/FailureResponse';
-import SuccessResponse from 'src/core/application/dto/common/responses/SuccessResponse';
-import InvalidModelStateResponse from 'src/core/application/dto/common/responses/InvalidModelStateResponse';
-import axios, { AxiosResponse } from 'axios';
-import NetworkException from 'src/core/application/common/exceptions/NetworkException';
-import { LoadingState } from 'src/core/application/common/atoms/global/LoadingState';
-
+import { RequestResponse } from "src/core/application/dto/common/responses/RequestResponse";
+import FailureResponse from "src/core/application/dto/common/responses/FailureResponse";
+import SuccessResponse from "src/core/application/dto/common/responses/SuccessResponse";
+import InvalidModelStateResponse from "src/core/application/dto/common/responses/InvalidModelStateResponse";
+import axios, { AxiosResponse } from "axios";
+import NetworkException from "src/core/application/common/exceptions/NetworkException";
+import { LoadingState } from "src/core/application/common/atoms/global/LoadingState";
 
 export default class RequestService implements IRequestService {
-    private readonly loggerService = new LoggerService;
-    private readonly baseURL = process.env.NEXT_PUBLIC_API_URL;
-    private readonly localStorageService = new LocalStorageService();
+  private readonly loggerService = new LoggerService();
+  private readonly baseURL = process.env.NEXT_PUBLIC_API_URL;
+  private readonly localStorageService = new LocalStorageService();
 
-    private getOptions(context: Cookie, file: boolean = false) {
-        let storage = this.localStorageService.readStorage(Constants.API_TOKEN_STORAGE);
-        let token = storage?.token || "";
-        let opts: any = {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            },
-            
-            // timeout: 1000,
-            // cancelToken: cancellationToken
-        };
+  private getOptions(context: Cookie, file: boolean = false) {
+    let storage = this.localStorageService.readStorage(
+      Constants.API_TOKEN_STORAGE
+    );
+    let token = storage?.token || "";
+    let opts: any = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
 
-        if (file) {
-            opts.responseType = "blob";
-        }
+      // timeout: 1000,
+      // cancelToken: cancellationToken
+    };
 
-        return opts;
-
+    if (file) {
+      opts.responseType = "blob";
     }
 
-     /**
-     * Convert AxiosResponse to app request
-     * @param response
-     */
-    private processRequest(response: AxiosResponse): RequestResponse {
-        try {
-            if (response.status == 200) {
-                return new SuccessResponse(response.statusText, response.data);
-            }
-            if (response.status == 202) {
-                return new FailureResponse(response.data.errors);
+    return opts;
+  }
 
-            }
-            if (response.status == 400) {
-                return new InvalidModelStateResponse(response.data.errors);
-            }
-            throw new NetworkException('No http status code handler');
-        } catch (e) {
-            new LoggerService().error(e);
-            throw e;
-        }
+  /**
+   * Convert AxiosResponse to app request
+   * @param response
+   */
+  private processRequest(response: AxiosResponse): RequestResponse {
+    try {
+      if (response.status == 200) {
+        return new SuccessResponse(response.statusText, response.data);
+      }
+      if (response.status == 202) {
+        return new FailureResponse(response.data.errors);
+      }
+      if (response.status == 400) {
+        return new InvalidModelStateResponse(response.data.errors);
+      }
+      throw new NetworkException("No http status code handler");
+    } catch (e) {
+      new LoggerService().error(e);
+      throw e;
     }
+  }
 
-    getCancellationToken() {
+  getCancellationToken() {}
 
+  async makeGetRequestAsync(
+    url: string,
+    params: any,
+    context: Cookie
+  ): Promise<RequestResponse> {
+    try {
+      const _params = params
+        ? Object.keys(params)
+            .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+            .join("&")
+        : "";
+      const _url = `${this.baseURL}/api/${url}${
+        _params === "" ? "" : "?" + _params
+      }`;
+      const _options = this.getOptions(context);
+      return this.processRequest(
+        await axios.get(_url, this.getOptions(context))
+      );
+    } catch (e) {
+      if (
+        e.response?.status == 400 ||
+        e.response?.status == 401 ||
+        e.response?.status == 500
+      ) {
+        this.localStorageService.setStorage(
+          Constants.API_TOKEN_STORAGE,
+          new Cookie(false, "", "")
+        );
+      }
+      // this.loggerService.error(e);
+      return new FailureResponse(e.response?.errors || e.errors);
+    } finally {
+      await setRecoilStateAsync(LoadingState, { isLoading: false, uri: "" });
     }
+  }
 
-    async makeGetRequestAsync(url: string, params: any, context: Cookie): Promise<RequestResponse> {
-        try {
-            const _params = params ? Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join("&") : "";
-            const _url = `${this.baseURL}/api/${url}${(_params === "" ? "" : "?" + _params)}`;
-            const _options = this.getOptions(context);
-            return this.processRequest(await axios.get(_url, this.getOptions(context)));
-        } catch (e) {
-            if(e.response?.status == 400 || e.response?.status == 401 || e.response?.status == 500){
-                this.localStorageService.setStorage(Constants.API_TOKEN_STORAGE, new Cookie(false, "", ""));
-                
-            }
-            // this.loggerService.error(e);
-            return new FailureResponse(e.response?.errors || e.errors);
-        }
-        finally {
-            await setRecoilStateAsync(LoadingState, { isLoading: false, uri: '' })
-        }
+  async makePostRequestAsync(
+    endpoint: string,
+    params: object,
+    context: Cookie
+  ): Promise<RequestResponse> {
+    //const setIsLoading = useSetRecoilState(LoadingState);
+    try {
+      const _url = `${this.baseURL}/api/${endpoint}`;
+      await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url });
+      const _params = JSON.stringify(params);
+      const _options = this.getOptions(context, false);
+      _options.headers["Content-Type"] = "application/json";
+      _options.headers["accept"] = "text/xml";
+      return this.processRequest(await axios.post(_url, _params, _options));
+    } catch (e) {
+      
+      // this.loggerService.error(e);
+      // throw e;
+      
+      if (
+        e.response?.status == 400 ||
+        e.response?.status == 401 ||
+        e.response?.status == 500
+      ) {
+        this.localStorageService.setStorage(
+          Constants.API_TOKEN_STORAGE,
+          new Cookie(false, "", "")
+        );
+      }
+
+      return new FailureResponse(e.response?.errors || e.errors);
+    } finally {
+      await setRecoilStateAsync(LoadingState, { isLoading: false, uri: "" });
     }
+  }
 
-    async makePostRequestAsync(endpoint: string, params: object, context: Cookie): Promise<RequestResponse> {
-        //const setIsLoading = useSetRecoilState(LoadingState);
-        try {
-            const _url = `${this.baseURL}/api/${endpoint}`;
-            await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url })
-            const _params = JSON.stringify(params);
-            const _options = this.getOptions(context, false);
-            _options.headers["Content-Type"] = 'application/json';
-            _options.headers["accept"] = 'text/xml';
-            return this.processRequest(await axios.post(_url, _params, _options));
-        } catch (e) {
-            // this.loggerService.error(e);
-            // throw e;
-
-            if(e.response?.status == 400 || e.response?.status == 401 || e.response?.status == 500){
-                this.localStorageService.setStorage(Constants.API_TOKEN_STORAGE, new Cookie(false, "", ""));
-            }
-
-            return new FailureResponse(e.response?.errors || e.errors);
-        }
-        finally {
-            await setRecoilStateAsync(LoadingState, { isLoading: false, uri: '' })
-        }
+  async makeGetFileRequestAsync(
+    url: string,
+    params: any,
+    context: Cookie
+  ): Promise<RequestResponse> {
+    try {
+      const _params = params
+        ? Object.keys(params)
+            .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+            .join("&")
+        : "";
+      const _url = `${this.baseURL}/api/${url}${
+        _params === "" ? "" : "?" + _params
+      }`;
+      const _options = this.getOptions(context);
+      return this.processRequest(
+        await axios.get(_url, this.getOptions(context, true))
+      );
+    } catch (e) {
+      // this.loggerService.error(e);
+      return new FailureResponse(e.response?.errors || e.errors);
+    } finally {
+      await setRecoilStateAsync(LoadingState, { isLoading: false, uri: "" });
     }
+  }
 
-    async makeGetFileRequestAsync(url: string, params: any, context: Cookie): Promise<RequestResponse> {
-        try {
-            const _params = params ? Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join("&") : "";
-            const _url = `${this.baseURL}/api/${url}${(_params === "" ? "" : "?" + _params)}`;
-            const _options = this.getOptions(context);
-            return this.processRequest(await axios.get(_url, this.getOptions(context, true)));
-        } catch (e) {
-            // this.loggerService.error(e);
-            return new FailureResponse(e.response?.errors || e.errors);
-        }
-        finally {
-            await setRecoilStateAsync(LoadingState, { isLoading: false, uri: '' })
-        }
+  async makeUploadRequestAsync(
+    endpoint: string,
+    file: File,
+    context: Cookie
+  ): Promise<RequestResponse> {
+    //const setIsLoading = useSetRecoilState(LoadingState);
+
+    try {
+      const _url = `${this.baseURL}/${endpoint}`;
+      await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url });
+      const _form = new FormData();
+      _form.append("file", file);
+      const _options = this.getOptions(context);
+      _options.headers["Content-Type"] = "multipart/form-data";
+      return this.processRequest(
+        await axios.post(_url, _form, this.getOptions(context))
+      );
+    } catch (e) {
+      // this.loggerService.error(e);
+      // throw e;
+      let result = (e as Error).message;
+      return new FailureResponse(result || e.errors);
+    } finally {
+      await setRecoilStateAsync(LoadingState, { isLoading: false, uri: "" });
     }
+  }
 
-    async makeUploadRequestAsync(endpoint: string, file: File, context: Cookie): Promise<RequestResponse> {
-        //const setIsLoading = useSetRecoilState(LoadingState);
-
-        try {
-            const _url = `${this.baseURL}/${endpoint}`;
-            await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url })
-            const _form = new FormData();
-            _form.append("file", file);
-            const _options = this.getOptions(context);
-            _options.headers["Content-Type"] = "multipart/form-data";
-            return this.processRequest(await axios.post(_url, _form, this.getOptions(context)));
-
-        } catch (e) {
-            // this.loggerService.error(e);
-            // throw e;
-            let result = (e as Error).message;
-            return new FailureResponse(result || e.errors);
-        }
-        finally {
-            await setRecoilStateAsync(LoadingState, { isLoading: false, uri: '' })
-        }
+  async makePutRequestAsync(
+    endpoint: string,
+    params: any,
+    context: Cookie
+  ): Promise<RequestResponse> {
+    //const setIsLoading = useSetRecoilState(LoadingState);
+    try {
+      const _url = `${this.baseURL}/api/${endpoint}`;
+      await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url });
+      const _params = JSON.stringify(params);
+      const _options = this.getOptions(context);
+      _options.headers["Content-Type"] = "application/json";
+      return this.processRequest(
+        await axios.put(_url, _params, this.getOptions(context))
+      );
+    } catch (e) {
+      // this.loggerService.error(e);
+      // throw e;
+      let result = (e as Error).message;
+      return new FailureResponse(result || e.errors);
+    } finally {
+      await setRecoilStateAsync(LoadingState, { isLoading: false, uri: "" });
     }
+  }
 
-    async makePutRequestAsync(endpoint: string, params: any, context: Cookie): Promise<RequestResponse> {
-        //const setIsLoading = useSetRecoilState(LoadingState);
-        try {
-            const _url = `${this.baseURL}/api/${endpoint}`;
-            await setRecoilStateAsync(LoadingState, { isLoading: true, uri: _url })
-            const _params = JSON.stringify(params);
-            const _options = this.getOptions(context);
-            _options.headers["Content-Type"] = 'application/json';
-            return this.processRequest(await axios.put(_url, _params, this.getOptions(context)));
-        } catch (e) {
-            // this.loggerService.error(e);
-            // throw e;
-            let result = (e as Error).message;
-            return new FailureResponse(result || e.errors);
-        }
-        finally {
-            await setRecoilStateAsync(LoadingState, { isLoading: false, uri: '' })
-        }
-    }
-
-
-    makeDeleteRequestAsync() {
-        axios.CancelToken.source().token
-    }
-
-
-
-    
-
+  makeDeleteRequestAsync() {
+    axios.CancelToken.source().token;
+  }
 }
